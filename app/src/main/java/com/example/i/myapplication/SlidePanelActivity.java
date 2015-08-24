c176fb184271d4a244d36a7a47054c3b426c24e4
su.wick <1553202383@qq.com>
2015-08-24 14:52:40 +0800

    提交代码

--- app/src/main/java/com/example/i/myapplication/SlidePanelActivity.java
+++ app/src/main/java/com/example/i/myapplication/SlidePanelActivity.java
@@ -1,9 +1,16 @@
 package com.example.i.myapplication;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
 
 /**
  * Created by Jason.Su on 2015/8/21.
  */
 public class SlidePanelActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.slidelayout);
+    }
 }
